a26f98ed60e78d5f3ea8bb210af09d6d83fa96f2
Marcos Hernangil <mhernangilp@gmail.com>
2024-10-30 12:20:39 +0100

    changed default config on constructor

--- config/ServerConfig.cpp
+++ config/ServerConfig.cpp
@@ -1,46 +1,11 @@
 #include "ServerConfig.hpp"
 #include <cstring>
-#
 
-
-ServerConfig::ServerConfig() : port(8001), host("127.0.0.1"), server_name("localhost"), client_max_body_size(1024), index("index.html"), root("docs/kebab_web/") {
-    error_page[404] = "custom_error/404.html";
-    
+ServerConfig::ServerConfig() : port(8002), host("127.0.0.1"), server_name("localhost"), client_max_body_size(1024), index("index.html"), root("docs/kebab_web/") {
     LocationConfig location = LocationConfig();
     location.location = "/";
     location.autoindex = "off";
-    location.allow_methods.push_back("DELETE");
-    location.allow_methods.push_back("POST");
     location.allow_methods.push_back("GET");
-    this->addLocation(location.location, location);
-    
-    location = LocationConfig();
-    location.location = "/kebabs";
-    location.autoindex = "on";
-    location.allow_methods.push_back("GET");
-    location.allow_methods.push_back("POST");
-    location.index = "kebab.html";
-    this->addLocation(location.location, location);
-
-    location = LocationConfig();
-    location.location = "/secret-sauce";
-    location.return_path = "/kebabs";
-    this->addLocation(location.location, location);
-
-    location = LocationConfig();
-    location.location = "/gallery";
-    location.root = "/fake-gallery";
-    location.index = "gallery.html";
-    location.allow_methods.push_back("GET");
-    this->addLocation(location.location, location);
-
-    location = LocationConfig();
-    location.location = "/cgi-bin";
-    location.root = "./";
-    location.allow_methods.push_back("GET");
-    location.allow_methods.push_back("POST");
-    location.allow_methods.push_back("DELETE");
-    location.index = "time.py";
     this->addLocation(location.location, location);
 }
 

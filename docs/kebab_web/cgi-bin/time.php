23eb024eff17aca93357f1f34d50a1284bc530d2
Gonzalo <gonchogfa@gmail.com>
2025-01-24 19:21:15 +0100

    style time

--- docs/kebab_web/cgi-bin/time.php
+++ docs/kebab_web/cgi-bin/time.php
@@ -1,7 +1,20 @@
 <?php
+
+echo "<html><head><style>
+    body {
+        font-family: Arial, sans-serif;
+        text-align: center;
+        margin-top: 50px;
+        color: #333;
+    }
+    h1 {
+        color: #4CAF50;
+    }
+</style></head><body>";
+
 // Establecer la zona horaria
 date_default_timezone_set('Europe/Madrid'); // Cambia esto según tu zona horaria
 
 // Mostrar la hora actual
-echo "La hora actual es: " . date("H:i:s");
+echo "<h1><strong>La hora actual es: </strong></h1>" . date("H:i:s");
 ?>
